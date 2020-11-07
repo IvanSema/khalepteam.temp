d510f6277afe93ba1b2d832c5de7f475bfb3c709
IvanSema <60390709+IvanSema@users.noreply.github.com>
2020-11-07 22:28:33 +0200

    current

--- js/ajax.js
+++ js/ajax.js
@@ -1,10 +1,10 @@
 function success(){
     const form = document.querySelector('.form');
-    form.innerHTML = '<h2 class="form__action">Спасибо, Ваша заявка в работе.</h2><br><span class="video-block__description">Ожидайте звонка менеджера.</span>'
+    form.innerHTML = '<h2 class="form__action">Спасибо, Ваша заявка в работе.</h2><br><span style="text-align: center;" class="video-block__description">Ожидайте звонка менеджера.</span>'
 }
 
 function fail(){
-    form.innerHTML = '<h2 class="form__action">Что-то пошло не так.</h2><br><span class="video-block__description">Обратитесь за номером: +380930323459</span>'
+    form.innerHTML = '<h2 class="form__action">Что-то пошло не так.</h2><br><span style="text-align: center;" class="video-block__description">Обратитесь за номером: +380930323459</span>'
 }
 
 
